497ae897d89555775b55a7b72ed37c86102bb794
kish117 <kish.siva@vivelacar.com>
2021-01-17 21:57:25 +0100

    allow broadcast to a room

--- src/index.js
+++ src/index.js
@@ -17,13 +17,17 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined!') )
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+    })
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
 
-        if(filter.isProfane(message)){
+        if (filter.isProfane(message)) {
             return callback('Profanity is not allowed!')
         }
 
@@ -31,15 +35,15 @@ io.on('connection', (socket) => {
         callback('Delivered!')
     })
 
-    socket.on('sendLocation', (coords, callback)=>{
+    socket.on('sendLocation', (coords, callback) => {
         io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
-    
-    socket.on('disconnect', ()=>{
+
+    socket.on('disconnect', () => {
         io.emit('message', generateMessage('A user has left!'))
     })
-    
+
 })
 
 server.listen(port, () => {
